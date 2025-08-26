7b38327ae0308ca35229ffb2eb99b27dac61d64c
Christian Trujillo <christian@superficial.design>
2025-08-25 18:13:53 -0600

    fade in

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="coming-soon">
+      <div className="coming-soon fade-in">
         <main>
           <ComingSoon/>
 
